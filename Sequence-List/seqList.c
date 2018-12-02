9c442694766f751c9cc3c47630b2ccae2dddf5fb
uscxiaoyu <jxndtbxiaoyu@163.com>
2018-12-02 14:33:37 +0800

    ubuntu

--- Sequence-List/seqList.c
+++ Sequence-List/seqList.c
@@ -13,3 +13,24 @@ seqList * init_SeqList(void){
     L->last = -1; // 等价于 (*L).last = -1
     return L;
 }
+
+// 在位置i插入整型值x
+int Insert_Seqlist(seqList *L, int i, int x){
+    int j;
+    if (L->last == MAXSIZE-1){
+        printf("表满");
+        return -1;
+    }
+    else if (i < 1 || i > L->last + 2){
+        printf("位置错");
+        return 0;
+    }
+
+    for (j=L->last; j>=i-1; j--){
+        L->data[j+1] = L->data[j];
+    }
+    L->data[i-1] = x;
+    L->last++;
+
+    return 1;
+}
